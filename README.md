b594aa24e2388c2f561f12d2df0d90bc6a12b3b7
Daniel Skowroński <daniel@dsinf.net>
2020-06-01 10:55:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# hetzner-auction-hunter
+checks for newest servers on Hetzner server auction (server-bidding) and pushes them via telegram_send
+
+https://www.hetzner.com/sb
+
+## requirements
+* python3
+* properly configured [telegram_send](https://pypi.org/project/telegram-send/#installation)
+* some witable file to store processed offers (defaulted to /tmp/hah.txt)
+
+## usage
+```
+usage: hah.py [-h] --price PRICE --disk-size DISK_SIZE [--disk-quick] --cpu-score CPU_SCORE --ram RAM [--ecc] [-f [F]]
+
+hah.py -- checks for newest servers on Hetzner server auction (server-bidding) and pushes them viatelegram_send
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --price PRICE         max price (€)
+  --disk-size DISK_SIZE
+                        min disk capacity (GB)
+  --disk-quick          require SSD/NVMe
+  --cpu-score CPU_SCORE
+                        min CPU benchmark score
+  --ram RAM             min RAM (GB)
+  --ecc                 require ECC memory
+  -f [F]                state file
+
+```
+
+Example: `./hah.py --price 51 --disk-size 3000 --ram 24 --cpu-score 10000` - this will get servers cheaper than 51 EUR with more than 24GB of RAM, disks at least 3TB and CPU with score better tan 10k.
+
+You'll probably want to put it in crontab.
---
+++ hah.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import requests, telegram_send, argparse
+
+parser = argparse.ArgumentParser(description='hah.py -- checks for newest servers on Hetzner server auction (server-bidding) and pushes them viatelegram_send')
+parser.add_argument('--price',      nargs=1,required=True,type=int,help='max price (€)')
+parser.add_argument('--disk-size',  nargs=1,required=True,type=int,help='min disk capacity (GB)')
+parser.add_argument('--disk-quick', action='store_true',           help='require SSD/NVMe')
+parser.add_argument('--cpu-score',  nargs=1,required=True,type=int,help='min CPU benchmark score')
+parser.add_argument('--ram',        nargs=1,required=True,type=int,help='min RAM (GB)')
+parser.add_argument('--ecc',        action='store_true',           help='require ECC memory')
+parser.add_argument('-f',           nargs='?',                     help='state file')
+args = parser.parse_args()
+
+if args.f==None:
+	file='/tmp/hah.txt'
+else:
+	file=args.f
+
+tax=1.19
+
+f = open(file,'a+')
+idsProcessed=open(file).read()
+
+r = requests.get('https://www.hetzner.com/a_hz_serverboerse/live_data.json')
+servers = r.json()['server']
+
+for server in servers:
+	if str(server['key']) in idsProcessed:
+		continue
+
+	price      = round(tax*float(server['price']),0)<=args.price[0]
+	disk_size  = server['hdd_size']>=args.disk_size[0]
+	disk_quick = server['is_highio'] if args.disk_quick else True
+	cpu_score  = server['cpu_benchmark']>=args.cpu_score[0]
+	ram        = server['ram']>=args.ram[0]
+	ecc        = server['is_ecc'] if args.ecc else True
+
+	if price and disk_size and disk_quick and cpu_score and ram and ecc:
+		msg="Hetzner server %.2f€: %dGB, %s (%d), %s HDD \nhttps://www.hetzner.com/sb ID: %d" % (tax*float(server['price']), server['ram'],server['cpu'],server['cpu_benchmark'],server['hdd_hr'],server['key'])
+		print(msg)
+		telegram_send.send(messages=[msg], parse_mode="markdown")
+		f.write(","+str(server['key']))
+
+f.close()
