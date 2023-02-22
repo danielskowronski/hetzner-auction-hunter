7c6c693071fd0245b7a1e0023db116758b43830f
Promises <21245147+Pr0mises@users.noreply.github.com>
2023-02-22 05:46:51 +0100

    fixed spaces

--- hah.py
+++ hah.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import httpx, json, telegram_send, argparse
+import httpx, json, telegram_send, argparse, os
 
 parser = argparse.ArgumentParser(description='hah.py -- unofficially checks for newest servers on Hetzner server auction (server-bidding) and pushes them via telegram_send')
 parser.add_argument('--tax',         nargs=1,required=False,type=int, help='tax rate (VAT) in percents, defaults to 19 (Germany)', default=[19])
 parser.add_argument('--price',       nargs=1,required=False,type=int, help='max price (€)')
 parser.add_argument('--disk-count',  nargs=1,required=False,type=int, help='min disk count', default=[1])
 parser.add_argument('--disk-size',   nargs=1,required=False,type=int, help='min disk capacity (GB)')
+parser.add_argument('--disk-min-size',   nargs=1,required=False,type=int, help='min disk capacity per disk (GB)')
 parser.add_argument('--disk-quick',  action='store_true',             help='require SSD/NVMe')
 parser.add_argument('--hw-raid',     action='store_true',             help='require Hardware RAID')
 parser.add_argument('--red-psu',     action='store_true',             help='require Redundant PSU')
@@ -27,13 +28,18 @@ parser.add_argument('--send-payload',action='store_true',             help='send
 args = parser.parse_args()
 
 if not args.test_mode:
-	if args.f==None:
-		file='/tmp/hah.txt'
-	else:
-		file=args.f
+  if args.f==None:
+    file='/tmp/hah.txt'
+  else:
+    file=args.f
 
-	f = open(file,'a+')
-	idsProcessed=open(file).read()
+  f = open(file,'a+')
+  tmp = open(file).read()
+  tmp.split(";")
+  serverProcessed = {}
+  if tmp:
+    for i in range(0, len(tmp)):
+      serverProcessed[i] = tmp[i]
 
 servers = None
 try:
@@ -44,79 +50,87 @@ except Exception as e:
     print('Failed to download auction list')
     print(e)
     exit(1)
-
+    
 for server in servers:
-	if args.debug:
-		print(json.dumps(server))
-	if not args.test_mode and str(server['key']) in idsProcessed:
-		continue
-        
-    sp_hw_raid=False
-	sp_red_psu=False
-	sp_ecc=False
-	sp_gpu=False
-	sp_ipv4=False
-	sp_inic=False
+  if args.debug:
+    print(json.dumps(server))
+  if not args.test_mode and bool(serverProcessed):
+    for id in serverProcessed:
+      if id['key'] is server['key'] and id['price'] is server['price']:
+        continue
 
-	for special in server['specials']:
-		if special=='HWR':
-			sp_hw_raid=True
-		if special=='RPS':
-			sp_red_psu=True
-		if special=='ECC':
-			sp_ecc=True
-		if special=='GPU':
-			sp_gpu=True
-		if special=='IPv4':
-			sp_ipv4=True
-		if special=='iNIC':
-			sp_inic=True
+  sp_hw_raid=False
+  sp_red_psu=False
+  sp_ecc=False
+  sp_gpu=False
+  sp_ipv4=False
+  sp_inic=False
 
-	datacenter=False if args.dc else True
+  for special in server['specials']:
+    if special=='HWR':
+      sp_hw_raid=True
+    if special=='RPS':
+      sp_red_psu=True
+    if special=='ECC':
+      sp_ecc=True
+    if special=='GPU':
+      sp_gpu=True
+    if special=='IPv4':
+      sp_ipv4=True
+    if special=='iNIC':
+      sp_inic=True
 
-	if args.dc is not None and args.dc[0] in server['datacenter']:
-		datacenter=True
+  datacenter=False if args.dc else True
 
-	exclude_tax = args.exclude_tax
-	price_value = round((100+args.tax[0])*float(server['price'])/100,0) if not exclude_tax else round((100)*float(server['price'])/100,0)
-	
-	price       = price_value<=args.price[0] if args.price else True
-	disk_count  = server['hdd_count']>=args.disk_count[0] if args.disk_count else True
-	disk_size   = server['hdd_size']>=args.disk_size[0] if args.disk_size else True
-	disk_quick  = (len(server['serverDiskData']['nvme'])+len(server['serverDiskData']['sata']) >0 ) if args.disk_quick else True
-	hw_raid     = sp_hw_raid if args.hw_raid else True
-	red_psu     = sp_red_psu if args.red_psu else True
-	cpu_count   = server['cpu_count']>=args.cpu_count[0] if args.cpu_count else True
-	ram         = server['ram_size']>=args.ram[0] if args.ram else True
-	ecc         = sp_ecc if args.ecc else True
-	gpu         = sp_gpu if args.gpu else True
-	ipv4        = sp_ipv4 if args.ipv4 else True
-	inic        = sp_inic if args.inic else True
+  if args.dc is not None and args.dc[0] in server['datacenter']:
+    datacenter=True
 
-	disk_descriptors=[desc for desc in server['description'] if " GB" in desc or " TB" in desc] # FIXME
-	if True or len(disk_descriptors)==0:
-		disk_description="%dx %dGB" % (server['hdd_count'], server['hdd_size'])
-	else:
-		disk_description=", ".join(disk_descriptors)
+  exclude_tax = args.exclude_tax
+  price_value = ((100+args.tax[0])*float(server['price'])/100) if not exclude_tax else ((100)*float(server['price'])/100)
+  
+  price       = price_value<=args.price[0] if args.price else True
+  disk_count  = server['hdd_count']>=args.disk_count[0] if args.disk_count else True
+  disk_size   = server['hdd_size']>=args.disk_size[0] if args.disk_size else True
+  disk_min_size = True
+  if args.disk_min_size and server['serverDiskData']['general']:
+    min_size = min(server['serverDiskData']['general'])
+    if min_size < args.disk_min_size[0]:
+      disk_min_size = False
 
-	if price and disk_count and disk_size and disk_quick and hw_raid and red_psu and cpu_count and ram and ecc and gpu and ipv4 and inic and datacenter:
-		msg="<b>Hetzner</b> server #%d for <b>%.2f€: %dGB RAM, %dx %s</b>, %s [DC: %s]\nhttps://www.hetzner.com/sb?search=%d" % \
-			(server['key'], price_value, server['ram_size'],server['cpu_count'],server['cpu'],disk_description,
-			server['datacenter'],server['key'])
-		print(msg)
+  disk_quick  = (len(server['serverDiskData']['nvme'])+len(server['serverDiskData']['sata']) >0 ) if args.disk_quick else True
+  hw_raid     = sp_hw_raid if args.hw_raid else True
+  red_psu     = sp_red_psu if args.red_psu else True
+  cpu_count   = server['cpu_count']>=args.cpu_count[0] if args.cpu_count else True
+  ram         = server['ram_size']>=args.ram[0] if args.ram else True
+  ecc         = sp_ecc if args.ecc else True
+  gpu         = sp_gpu if args.gpu else True
+  ipv4        = sp_ipv4 if args.ipv4 else True
+  inic        = sp_inic if args.inic else True
+  
+  disk_descriptors=[desc for desc in server['information'] if " GB" in desc or " TB" in desc] # FIXME
+  if len(disk_descriptors)==0:
+    disk_description="%dx %dGB" % (server['hdd_count'], server['hdd_size'])
+  else:
+    disk_description=", ".join(disk_descriptors)
+ 
+  if price and disk_count and disk_size and disk_min_size and disk_quick and hw_raid and red_psu and cpu_count and ram and ecc and gpu and ipv4 and inic and datacenter:
+    msg="<b>Hetzner</b> server #%d for <b>%.2f€: %dGB RAM, %dx %s</b>, %s [DC: %s]\nhttps://www.hetzner.com/sb?search=%d" % \
+      (server['key'], price_value, server['ram_size'],server['cpu_count'],server['cpu'],disk_description,
+      server['datacenter'],server['key'])
+    print(msg)
 
-		if args.send_payload:
-			print(json.dumps(server))
-			msg+=" <pre>"+json.dumps(server)+"</pre>"
+    if args.send_payload:
+      print(json.dumps(server))
+      msg+=" <pre>"+json.dumps(server)+"</pre>"
 
 
-		if not args.test_mode:
-			if args.tgm_config:
-				telegram_send.send(messages=[msg], parse_mode="html", conf=args.tgm_config[0])
-			else:
-				telegram_send.send(messages=[msg], parse_mode="html")
-
-			f.write(","+str(server['key']))
+    if not args.test_mode and not args.debug:
+      if args.tgm_config:
+        telegram_send.send(messages=[msg], parse_mode="html", conf=args.tgm_config[0])
+      else:
+        telegram_send.send(messages=[msg], parse_mode="html")
+      
+      f.write("key:" + str(server['key']) + ",price:"+ str(server['price']) + ";")
 
 if not args.test_mode:
-	f.close()
+  f.close()
