8803fff620053760bcff2078e79a4576a493cbcc
Promises <21245147+Pr0mises@users.noreply.github.com>
2023-02-23 06:13:37 +0100

    added disk-min-size per disk, ipv4-price and price update push msg
    New try, now everything should work as intended
    
    The data now gets stored as {"key":"1998135","price":"36"}; therefore a new push is also generated if the price changes for an existing server and the new price get's replaced in the hash file.
    
    Well I didn't see you're working on v2, maybe those edits could be handy for v2 as well.
    Maybe consider to use 2 spaces instead of tabs, was kinda hard to revert it :D
    
    Added:
    - disk-min-size per drive
    - push msg at price change (using dict)
    - deleted round in price_value and adding + ipv4 price for exact pricing
    - You can now set your ipv4 price (default is 2.02€)
    - separate msg for new price or new server
    Anyway thanks for this project and maybe some changes are useful for you

--- hah.py
+++ hah.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import httpx, json, telegram_send, argparse
+import httpx, json, telegram_send, argparse, os
 
 parser = argparse.ArgumentParser(description='hah.py -- unofficially checks for newest servers on Hetzner server auction (server-bidding) and pushes them via telegram_send')
 parser.add_argument('--tax',         nargs=1,required=False,type=int, help='tax rate (VAT) in percents, defaults to 19 (Germany)', default=[19])
+parser.add_argument('--ipv4-price',       nargs=1,required=False,type=float, help='price for ipv4 (€) (check in order)', default=[2.02])
 parser.add_argument('--price',       nargs=1,required=False,type=int, help='max price (€)')
 parser.add_argument('--disk-count',  nargs=1,required=False,type=int, help='min disk count', default=[1])
 parser.add_argument('--disk-size',   nargs=1,required=False,type=int, help='min disk capacity (GB)')
+parser.add_argument('--disk-min-size',   nargs=1,required=False,type=int, help='min disk capacity per disk (GB)')
 parser.add_argument('--disk-quick',  action='store_true',             help='require SSD/NVMe')
 parser.add_argument('--hw-raid',     action='store_true',             help='require Hardware RAID')
 parser.add_argument('--red-psu',     action='store_true',             help='require Redundant PSU')
@@ -33,7 +35,15 @@ if not args.test_mode:
 		file=args.f
 
 	f = open(file,'a+')
-	idsProcessed=open(file).read()
+	serverProcessed = {}
+	# check for file size == 0 cause open file is weird...
+	if os.path.getsize(file) != 0:
+		# data is not empty even if the file is empty?? i don't get it
+		data = open(file).read()
+		# print(f"data: {repr(data)}") shows data: '' and "if data != '':"" does not work
+		data = data.split(";")
+		for i in range(0, len(data)-1):
+			serverProcessed[i] = json.loads(data[i])
 
 servers = None
 try:
@@ -44,12 +54,31 @@ except Exception as e:
     print('Failed to download auction list')
     print(e)
     exit(1)
-
+		
 for server in servers:
+	bNew_price = False
+	old_price_value = 0
+	old_price = 0
 	if args.debug:
 		print(json.dumps(server))
-	if not args.test_mode and str(server['key']) in idsProcessed:
-		continue
+	if not args.test_mode and bool(serverProcessed):
+		bContinue_to_next_server = False
+		for key, value in serverProcessed.items():
+			if int(value['key']) == int(server['key']):
+				if int(value['price']) == int(server['price']):
+					bContinue_to_next_server = True
+					break
+				else:
+					# print("we got a new price :)")
+					bNew_price = True
+					old_price = int(value['price'])
+					if sp_ipv4:
+						old_price_value = (((100+args.tax[0])*float(value['price'])/100)+ args.ipv4_price[0]) if not exclude_tax else ((100)*float(value['price'])/100)
+					else:
+						old_price_value = ((100+args.tax[0])*float(value['price'])/100) if not exclude_tax else ((100)*float(value['price'])/100)
+					f.close() # close file to free space as it will be read again at the bottom
+		if bContinue_to_next_server:
+			continue
 
 	sp_hw_raid=False
 	sp_red_psu=False
@@ -78,31 +107,48 @@ for server in servers:
 		datacenter=True
 
 	exclude_tax = args.exclude_tax
-	price_value = round((100+args.tax[0])*float(server['price'])/100,0) if not exclude_tax else round((100)*float(server['price'])/100,0)
+	if sp_ipv4:
+		price_value = (((100+args.tax[0])*float(server['price'])/100)+ args.ipv4_price[0]) if not exclude_tax else ((100)*float(server['price'])/100)
+	else:
+		price_value = ((100+args.tax[0])*float(server['price'])/100) if not exclude_tax else ((100)*float(server['price'])/100)
 	
-	price       = price_value<=args.price[0] if args.price else True
-	disk_count  = server['hdd_count']>=args.disk_count[0] if args.disk_count else True
-	disk_size   = server['hdd_size']>=args.disk_size[0] if args.disk_size else True
-	disk_quick  = (len(server['serverDiskData']['nvme'])+len(server['serverDiskData']['sata']) >0 ) if args.disk_quick else True
-	hw_raid     = sp_hw_raid if args.hw_raid else True
-	red_psu     = sp_red_psu if args.red_psu else True
-	cpu_count   = server['cpu_count']>=args.cpu_count[0] if args.cpu_count else True
-	ram         = server['ram_size']>=args.ram[0] if args.ram else True
-	ecc         = sp_ecc if args.ecc else True
-	gpu         = sp_gpu if args.gpu else True
-	ipv4        = sp_ipv4 if args.ipv4 else True
-	inic        = sp_inic if args.inic else True
+	price         = price_value<=args.price[0] if args.price else True
+	disk_count    = server['hdd_count']>=args.disk_count[0] if args.disk_count else True
+	disk_size     = server['hdd_size']>=args.disk_size[0] if args.disk_size else True
+	disk_min_size = True
+	
+	if args.disk_min_size and server['serverDiskData']['general']:
+		min_size    = min(server['serverDiskData']['general'])
+		if min_size < args.disk_min_size[0]:
+			disk_min_size = False
 
-	disk_descriptors=[desc for desc in server['description'] if " GB" in desc or " TB" in desc] # FIXME
-	if True or len(disk_descriptors)==0:
+	disk_quick    = (len(server['serverDiskData']['nvme'])+len(server['serverDiskData']['sata']) >0 ) if args.disk_quick else True
+	hw_raid       = sp_hw_raid if args.hw_raid else True
+	red_psu       = sp_red_psu if args.red_psu else True
+	cpu_count     = server['cpu_count']>=args.cpu_count[0] if args.cpu_count else True
+	ram           = server['ram_size']>=args.ram[0] if args.ram else True
+	ecc           = sp_ecc if args.ecc else True
+	gpu           = sp_gpu if args.gpu else True
+	ipv4          = sp_ipv4 if args.ipv4 else True
+	inic          = sp_inic if args.inic else True
+	
+	disk_descriptors=[desc for desc in server['information'] if " GB" in desc or " TB" in desc] # FIXME
+	if len(disk_descriptors)==0:
 		disk_description="%dx %dGB" % (server['hdd_count'], server['hdd_size'])
 	else:
 		disk_description=", ".join(disk_descriptors)
-
-	if price and disk_count and disk_size and disk_quick and hw_raid and red_psu and cpu_count and ram and ecc and gpu and ipv4 and inic and datacenter:
-		msg="<b>Hetzner</b> server #%d for <b>%.2f€: %dGB RAM, %dx %s</b>, %s [DC: %s]\nhttps://www.hetzner.com/sb?search=%d" % \
+ 
+	if price and disk_count and disk_size and disk_min_size and disk_quick and hw_raid and red_psu and cpu_count and ram and ecc and gpu and ipv4 and inic and datacenter:
+		msg=""
+		if bNew_price:
+			price_diff = price_value - old_price_value
+			msg="<b>Hetzner</b> server #%d for <b>%.2f€ (%.2f€): %dGB RAM, %dx %s</b>, %s [DC: %s]\nhttps://www.hetzner.com/sb?search=%d" % \
+				(server['key'], price_value, price_diff, server['ram_size'],server['cpu_count'],server['cpu'],disk_description,
+				server['datacenter'],server['key'])
+		else:
+			msg="<b>Hetzner</b> server #%d for <b>%.2f€: %dGB RAM, %dx %s</b>, %s [DC: %s]\nhttps://www.hetzner.com/sb?search=%d" % \
 			(server['key'], price_value, server['ram_size'],server['cpu_count'],server['cpu'],disk_description,
-			server['datacenter'],server['key'])
+			server['datacenter'],server['key'])  
 		print(msg)
 
 		if args.send_payload:
@@ -110,13 +156,21 @@ for server in servers:
 			msg+=" <pre>"+json.dumps(server)+"</pre>"
 
 
-		if not args.test_mode:
+		if not args.test_mode and not args.debug:
 			if args.tgm_config:
 				telegram_send.send(messages=[msg], parse_mode="html", conf=args.tgm_config[0])
 			else:
 				telegram_send.send(messages=[msg], parse_mode="html")
+			if not bNew_price:
+				f.write('{"key":"' + str(server['key']) + '","price":"'+ str(server['price']) + '"};')
+			else:
+				# replace new price with old price
+				with open(file,'r') as f:
+					replace = f.read()
+					replace = replace.replace('{"key":"' + str(server['key']) + '","price":"'+ str(old_price) + '"};', '{"key":"' + str(server['key']) + '","price":"'+ str(server['price']) + '"};')
+				with open(file, 'w') as f:
+					f.write(replace)
 
-			f.write(","+str(server['key']))
 
 if not args.test_mode:
 	f.close()
